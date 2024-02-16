db8f94b9db0021d7c69e11c2ff76b2ad25c796cf
Okayami <46897189+Okayami@users.noreply.github.com>
2024-02-16 11:29:57 +0100

    zsss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const routes:Routes = [
+  {path:'about-us', component:AboutUsComponent},
+  {path:'how-it-works', component:HowItWorksComponent}
+];
 
 @NgModule({
   declarations: [
@@ -11,8 +18,11 @@ import { HowItWorksComponent } from './how-it-works/how-it-works.component';
     AboutUsComponent,
     HowItWorksComponent
   ],
+  exports:[RouterModule],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
